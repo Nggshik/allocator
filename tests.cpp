7bb477774c023cd3d100f201c9bf1a913745208b
Baranovsky S.A <baranovsky.s.a@gmail.com>
2021-03-10 14:06:43 +0300

    Test case added for custom linkedlist

--- tests.cpp
+++ tests.cpp
@@ -1,12 +1,23 @@
 #define BOOST_TEST_MODULE allocator
 
 #include <boost/test/unit_test.hpp>
+#include "linkedlist.hpp"
 
 BOOST_AUTO_TEST_SUITE(Allocate_test)
 
-BOOST_AUTO_TEST_CASE(allocate)
+BOOST_AUTO_TEST_CASE(custom_container_test)
 {
-    BOOST_CHECK(1==1);
+    linkedlist<int> ll;
+    for(size_t i = 0; i < 10; ++i)
+    {
+        ll.emplace(i);
+    }
+    
+    size_t check_val = 0;
+    for(auto& val : ll)
+    { 
+        BOOST_CHECK(val==check_val++);
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
